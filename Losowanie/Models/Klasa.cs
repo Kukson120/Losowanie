99afd2fa2dd34ea71d769b9d0f9269746e5bc43e
MArcel <MArcel@DESKTOP-OU05MR6>
2024-04-17 23:49:36 +0200

    Dodaj pliki projektów.

---
+++ Losowanie/Models/Klasa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Losowanie.Models
+{
+    internal class Klasa
+    {
+        public string Filename { get; set; }
+        public string ClassNumber { get; set; }
+        public string Text { get; set; }
+    }
+}
---
+++ Losowanie/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Losowanie.Models
+{
+    internal class Student
+    {
+        public int Number { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string ClassNumber { get; set; }
+    }
+}
